4bfaec27d6dfd471b11dc1947e4f542fadfe556e
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-02-03 15:46:02 -0300

    Adiciona projeto base do Xcode

---
+++ baforada/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  baforada
+//
+//  Created by José Henrique Fernandes Silva on 03/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
